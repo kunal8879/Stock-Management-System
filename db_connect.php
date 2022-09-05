06c239c1eb7e8464fbac23a24d2f52fca2ecff84
Kunal8879 <kunalsaini8879@gmail.com>
2022-09-05 19:00:15 +0530

    complete login page and connect to database

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$localhost = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "stock";
+
+$connect = new mysqli($localhost, $username, $password, $dbname);
+
+if ($connect->connect_error) {
+    die("Connection Failed: " . $connect->connect_error);
+} else {
+    echo "Successfully Connected";
+}
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require_once 'db_connect.php';
+    $sql = "SELECT * FROM  users WHERE username = '$username' AND `password` = '$_password'";
+    $result = $conn->query($sql);
+
+    if ($result == true) {
+        if (!($result->num_rows == 1)) {
+            $_SESSION['error'] = 'Wrong Credentials !!!';
+            header('location: ../index.php');
+        } else {
+            $row = $result->fetch_assoc();
+            if ($row['role'] == 1) {
+                $_SESSION['username'] = 'ADMIN';
+                $_SESSION['user_id'] = $row['dept_id'];
+                $_SESSION['role'] = 'admin';
+                $_SESSION['success'] = 'Welcome ' . $row['username'] . ' to admin home page';
+
+                header('location: ../home_page.php');
+            } else {
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['user_id'] = $row['user_id'];
+                $_SESSION['role'] = 'lab administrator';
+                $_SESSION['success'] = 'Welcome ' . $row['username'] . ' to lab home page';
+
+                header('location: ../home_page.php');
+            }
+        }
+    }
+    //close connection
+    $conn->close();
+} else {
+    $_SESSION['error'] = 'Something went wrong';
+    header('location: ../index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div id="bg"></div>
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" id="loginform">
+        <div class="form-field">
+            <input type="text" placeholder="Username" id="username" name="username" required />
+        </div>
+
+        <div class="form-field">
+            <input type="password" placeholder="Password" id="password" name="password" required />
+        </div>
+
+        <div class="form-field">
+            <input type="submit" name="submit" value="LOGIN" class="btn">
+        </div>
+        </div>
+    </form>
+
+</body>
+
+</html>
