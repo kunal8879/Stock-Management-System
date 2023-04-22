bced346d5cc83b85865f64e464d5b9bf80d33e07
Kunal8879 <kunalsaini8879@gmail.com>
2023-04-23 00:06:50 +0530

    changes password and username of database

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "mysql.cwfdvw0pptzn.us-east-1.rds.amazonaws.com";
-$username = "root";
-$password = "";
+$username = "admin";
+$password = "mysql8879";
 $dbname = "stock";
 
 $conn = new mysqli($host, $username, $password, $dbname);
